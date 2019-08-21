5592d619f2e9a6e3207cc8bd695514d3c6a7c89c
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2019-08-21 18:03:03 -0300

    Criação de middleware para verificar a existencia dos projetos, criação de middleware global para logar quantidade de requests feitas.

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+let totalRequests = 0;
 const projects = [];
+
+server.use((request, response, next) => {
+  totalRequests++;
+  console.log(`Total requests: ${totalRequests}`);
+
+  next();
+});
+
+function checkProjectExists(request, response, next) {
+  const { id } = request.params;
+  const project = projects.find(p => p.id === id);
+
+  if (!project) {
+    return response
+      .status(400)
+      .json({ error: `Project id:${id} does not exists.` });
+  }
+
+  next();
+}
 
 function returnAllProjects(response) {
   response.json(projects);
@@ -29,7 +50,7 @@ server.post("/projects", (request, response) => {
 });
 
 // Alterar um projeto
-server.put("/projects/:id", (request, response) => {
+server.put("/projects/:id", checkProjectExists, (request, response) => {
   const { id } = request.params;
   const { title } = request.body;
 
@@ -40,7 +61,7 @@ server.put("/projects/:id", (request, response) => {
 });
 
 // Deletar um projeto
-server.delete("/projects/:id", (request, response) => {
+server.delete("/projects/:id", checkProjectExists, (request, response) => {
   const { id } = request.params;
 
   const index = projects.findIndex(p => p.id === id);
@@ -50,7 +71,7 @@ server.delete("/projects/:id", (request, response) => {
 });
 
 // Incluir uma task
-server.post("/projects/:id/tasks", (request, response) => {
+server.post("/projects/:id/tasks", checkProjectExists, (request, response) => {
   const { id } = request.params;
   const { title } = request.body;
   const project = projects.find(p => p.id === id);
